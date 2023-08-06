c118c509e04bc36e86f24f70ade869549432d0f9
Soe Kyaw Thu <soekyawthu.dev@gmail.com>
2023-08-06 09:53:59 +0630

    Added Catalog.API Project

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CatalogController : ControllerBase
+{
+    private readonly ILogger<CatalogController> _logger;
+
+    public CatalogController(ILogger<CatalogController> logger)
+    {
+        _logger = logger;
+    }
+}
