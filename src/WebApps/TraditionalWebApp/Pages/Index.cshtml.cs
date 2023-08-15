bf2120b3bec0606a8b7ca871e1a449477c8d1bcc
Soe Kyaw Thu <soekyawthu.dev@gmail.com>
2023-08-15 23:42:58 +0630

    Added Additional Files into Traditional Web App

---
+++ src/WebApps/TraditionalWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TraditionalWebApp.Models;
+using TraditionalWebApp.Services;
+
+namespace TraditionalWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly ICatalogService _catalogService;
+    private readonly IBasketService _basketService;
+
+    public IndexModel(ILogger<IndexModel> logger, ICatalogService catalogService, IBasketService basketService)
+    {
+        _logger = logger;
+        _catalogService = catalogService;
+        _basketService = basketService;
+    }
+
+    public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
+
+    public async Task<IActionResult> OnGet()
+    {
+        ProductList = await _catalogService.GetCatalog();
+        return Page();
+    }
+    
+    public async Task<IActionResult> OnPostAddToCartAsync(string productId)
+    {
+        var product = await _catalogService.GetCatalog(productId);
+
+        const string userName = "swn";
+        var basket = await _basketService.GetBasket(userName);
+
+        if (basket is null || product is null)
+            return Page();
+
+        basket.Items.Add(new BasketItemModel
+        {
+            ProductId = productId,
+            ProductName = product.Name,
+            Price = product.Price,
+            Quantity = 1,
+            Color = "Black"
+        });
+
+        await _basketService.UpdateBasket(basket);
+        return RedirectToPage("Cart");
+    }
+}
---
+++ src/WebApps/TraditionalWebApp/Program.cs
@@ -0,0 +1,35 @@
+using TraditionalWebApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(client => 
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayUrl"]!));
+
+builder.Services.AddHttpClient<IBasketService, BasketService>(client => 
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayUrl"]!));
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(client => 
+    client.BaseAddress = new Uri(builder.Configuration["ApiSettings:GatewayUrl"]!));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
